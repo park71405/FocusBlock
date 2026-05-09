464e571b3708f96ee8a81d349975d2b9245954a1
박예슬 <park71403@gmail.com>
2026-05-09 15:05:55 +0900

    spring boot starter 적용

---
+++ focusblock-api/src/main/java/com/parkdev/focusbolck/FocusbolckApplication.java
@@ -0,0 +1,13 @@
+package com.parkdev.focusbolck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusbolckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusbolckApplication.class, args);
+	}
+
+}
---
+++ focusblock-api/src/test/java/com/parkdev/focusbolck/FocusbolckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkdev.focusbolck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusbolckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
